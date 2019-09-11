ef2a8853aed16e345b2d9ba3652155850acc794c
bladelores <bladelores.pls@gmail.com>
2019-09-11 15:03:35 +0300

    Update tf-state.tf

--- terraform/env-campaigninfo-production-infrastructure/tf-state.tf
+++ terraform/env-campaigninfo-production-infrastructure/tf-state.tf
@@ -3,7 +3,6 @@ terraform {
     region = "eu-central-1"
     bucket = "campaigninfo-prod-infrastructure"
     key = "terraform/campaigninfo/eu-central-1/prod/infrastructure.tfstate"
-   #kms_key_id = "arn:aws:kms:eu-central-1:551556101011:key/12e138ce-555b-476c-8b8c-af7670cf9f20"
     encrypt = true
   }
 }
