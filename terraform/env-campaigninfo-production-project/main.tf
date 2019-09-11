7d4fa31ba109699ef0aa9733b19b095ce5e6b8eb
bladelores <bladelores.pls@gmail.com>
2019-09-11 15:02:55 +0300

    Update main.tf

--- terraform/env-campaigninfo-production-project/main.tf
+++ terraform/env-campaigninfo-production-project/main.tf
@@ -3,7 +3,5 @@ module "campaigninfo" {
   AWS_REGION        = "eu-central-1"
   ENVIRONMENT       = "prod"
   RESOURCE_PREFIX   = "campaigninfo"
-  SSL_ARN           = "arn:aws:acm:eu-central-1:551556101011:certificate/fba6e837-0f0b-4bd1-aa6f-90519c8bd04b"
-  TERRAFORM_KMS_KEY = "arn:aws:kms:eu-central-1:551556101011:key/12e138ce-555b-476c-8b8c-af7670cf9f20"
   DOMAIN            = "goodyear.eu"
 }
